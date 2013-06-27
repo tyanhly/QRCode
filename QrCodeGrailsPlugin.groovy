9d4b15e1150b4be35724285351bb147cc81d663f
Tung Ly <tyanhly@yahoo.com>
2013-06-27 08:20:49 +0700

    Update QrCodeGrailsPlugin.groovy
    Change license, scm, issueManagement ect, fields populated

--- QrCodeGrailsPlugin.groovy
+++ QrCodeGrailsPlugin.groovy
@@ -25,19 +25,20 @@ which you can put your logo to this image.
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def organization = [ name: "KiSS Concept", url: "http://kiss-concept.com" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ [ name: "Tung Ly", email: "tung.ly@kiss-concept.com" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GRAILS-10200" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    
+    def scm = [ url: "https://github.com/tyanhly/QRCode.git" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
