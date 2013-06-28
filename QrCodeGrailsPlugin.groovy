634c3a0a3a33baedc0c667f5979e1b1ffa1bba9c
Tung Ly <tyanhly@yahoo.com>
2013-06-28 13:50:24 +0700

    Edit document link

--- QrCodeGrailsPlugin.groovy
+++ QrCodeGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class QrCodeGrailsPlugin {
-    def version = "0.1"
+    def version = "0.1.1"
     def grailsVersion = "2.0 > *"
 
     def title = "QRCode Plugin"
@@ -7,7 +7,7 @@ class QrCodeGrailsPlugin {
     def authorEmail = "tyanhly@gmail.com"
     def description = 'Allows your Grails application to create QRCode image which you can put your logo to this image'
 
-    def documentation = "http://grails.org/plugin/qr-code"
+    def documentation = "https://github.com/tyanhly/QRCode"
 
     def license = "APACHE"
     def issueManagement = [system: 'GITHUB', url: 'https://github.com/tyanhly/QRCode/issues']
